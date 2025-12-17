9a12aa6943aeca6c93f2e9ffbee01b556d0279d3
Vaishnavi Giri <vaishnavigiri1210@gmail.com>
2025-12-17 15:19:08 +0530

    updated

---
+++ Assignment/week 1/datastructure.py
@@ -0,0 +1,8 @@
+#reading and using elements
+
+arr=[]
+n=int(input('How many elements you can store:'))
+for x in range(n):
+    num=int(input('Enter Number:'))
+    arr.append(num)
+print('List is:',arr)
---
+++ Python/list.py
@@ -0,0 +1,79 @@
+#list
+
+a=[4,1,43,21,67,4,89,20,4,92,61,76]
+print(a)
+
+#built in functions
+print(len(a))
+print(min(a))
+print(max(a))
+print(sum(a))
+print(sorted(a))
+
+list('mnp')
+print(list)
+data=[0,0,0,0,False]
+print(any(data))
+b=[0,0,0,3,False]
+print(any(b))
+print(all(b))
+print(all(a))
+print(a.index(5))
+print(a.count(4))
+
+#built-in methods
+print(a.append(0))
+print(a.insert(1,22))
+print(a.remove(5))
+w=a.pop()
+print(w)
+print(a)
+print(a.sort())
+print(a.reverse())
+
+num=[10,20,30,40,50,60]
+print(num)
+
+#assigninng list elements
+x,y,z=a
+print(x)
+print(y)
+print(z)
+
+#Slicing
+print(num[2:4])
+print(num[2:])
+print(num[:5])
+print(num[:])
+
+#replacing set of elements
+num[3:6]=['hello','world']
+print(num)
+
+#existence of element using 'in'
+print(80 in num)
+print('hello' in num)
+print(40 not in num)
+
+#Arithmetic Operations
+print(num*2)
+print(num +[4,8])
+
+#iterating through list
+#odd no
+for n in num:
+    if n % 2!=0:
+        print(n,end='')
+
+#deleting elements
+del num[2:5]
+print(num)
+# del num
+# print(num)
+
+#multi-dimensional list
+mat=[[1,2,3][4,5,6][7,[8,9]]]
+print(mat)
+print(mat[2])
+print(mat[2][1])
+print(mat[2][1][0])
