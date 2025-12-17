52cb3caf34acc9647e379fd11960817f46899ef6
Vaishnavi Giri <vaishnavigiri1210@gmail.com>
2025-12-17 13:02:45 +0530

    loop code and notes added

---
+++ Python/loop.py
@@ -0,0 +1,22 @@
+# while loop
+
+"""count=0
+while count < 5:
+    print("Vaishnavi")
+    count=count+1
+print("======================") """
+
+# for loop
+
+""" for i in range(5):
+    print(i)
+print("======================") """
+
+""" for i in range(0,10):
+    print(i)
+print("======================") """
+
+departments=["IT","CO","AIML","ENTC"]
+for dep in departments:
+    print(dep)
+print("======================")
