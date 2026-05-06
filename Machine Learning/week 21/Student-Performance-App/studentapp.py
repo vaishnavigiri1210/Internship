ea46246e2b3f5a2a7298a69d6803b360c8d6284a
Vaishnavi Giri <vaishnavigiri1210@gmail.com>
2026-05-06 14:26:49 +0530

    updated student app

--- Machine Learning/week 21/Student-Performance-App/studentapp.py
+++ Machine Learning/week 21/Student-Performance-App/studentapp.py
@@ -5,8 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("🔥 TEST UPDATE 🔥")
-
 # ---------------- CONFIG ----------------
 st.set_page_config(page_title="Student Predictor", page_icon="🎓", layout="centered")
 
